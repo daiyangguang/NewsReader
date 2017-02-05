911e2a78ce142e0e0be686d150ee403e52a4a237
daiyanggguang <you@example.com>
2017-02-05 11:45:47 +0800

    Define contants

--- NewsReader/Defines.swift
+++ NewsReader/Defines.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2017年 suisuiyi. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+    // 设备类型判断
+    let  IsiPad   =  (UI_USER_INTERFACE_IDIOM() == .pad)
+    let  IsiPhone =  (UI_USER_INTERFACE_IDIOM() == .phone)
+    let  IsRetain =  UIScreen.main.scale >= 2
+    let  ScreenWidth = UIScreen.main.bounds.size.width
+    let  ScreenHeight = UIScreen.main.bounds.size.height
+    let  ScreenMaxLength = max(ScreenWidth, ScreenHeight)
+    let  ScreenMinHeight = min(ScreenWidth, ScreenHeight)
+    
+    let IsiPhone4  = (IsiPhone && ScreenMaxLength < 568.0)
+    let IsiPhone5  = (IsiPhone && ScreenMaxLength == 568.0)
+    let IsiPhone6  = (IsiPhone && ScreenMaxLength == 667.0)
+    let IsiPhone6P = (IsiPhone && ScreenMaxLength == 736.0)
+
